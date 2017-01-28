47411fa52dfb074e3e3c1d8aa75977a47ab790c8
Batya Zamansky <Batya Zamansky>
2017-01-27 21:56:29 -0500

    pulling pokemon

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
 window.onload = function() {
-	
+	console.log(pullPokemon(10));
+}
+
+// pull x random pokemon from the API
+function pullPokemon(x){
+	var pokemon = [];
+	for (var i = 1; i <= x; i++) {
+		var data = $.getJSON("http://pokeapi.co/api/v2/pokemon/" + x);
+		pokemon.append(data);
+	}
+	return pokemon;
 }
